151123ab88ace06410c93836731d66bdd1d1eaf2
Michelangelo Giacomelli <michelangelog@gmail.com>
2026-08-08 16:04:11 +0200

    modificato

---
+++ capitolo-02/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>La mia Home Page</title>
+</head>
+<body>
+    <h1>Benvenuto nel mio sito Laravel!</h1>
+    <p>Questo è un file HTML gestito da Blade.</p>
+</body>
+</html>
---
+++ capitolo-02/storage/framework/views/c13ec6468e2b326509359df66e1fdd4f.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>La mia Home Page</title>
+</head>
+<body>
+    <h1>Benvenuto nel mio sito Laravel!</h1>
+    <p>Questo è un file HTML gestito da Blade.</p>
+</body>
+</html>
+<?php /**PATH /Users/michelangelo/dev/php/corso-laravel/capitolo-02/resources/views/home.blade.php ENDPATH**/ ?>
